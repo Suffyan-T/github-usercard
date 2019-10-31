361ed03fd7a721689f2594e8fc284c9c0f64fd31
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-10-31 11:11:23 -0700

    Stretch > chained axios requests

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,6 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/Suffyan-T')
-  .then( data => {
-    console.log(data.data);
-  })
-  .catch (err => {
-    console.log(err.message);
-  })
-  
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -22,75 +14,6 @@ axios.get('https://api.github.com/users/Suffyan-T')
            create a new component and add it to the DOM as a child of .cards
 */
 
-function cardCreator(user) {
-  const div = document.createElement('div');
-  div.classList.add('card');
-
-  const image = document.createElement('img');
-  image.src = user.avatar_url; 
-  div.appendChild(image);
-  image.classList.add('card-img');
-  
-  const infoCont = document.createElement('div');
-  infoCont.classList.add('card-info');
-  div.appendChild(infoCont);
-
-  const cardName = document.createElement('h3');
-  cardName.classList.add('card-name');
-  cardName.textContent = user.name;
-  infoCont.appendChild(cardName);
-
-  const userName = document.createElement('p');
-  userName.classList.add('card-username');
-  userName.textContent = user.login; 
-  infoCont.appendChild(userName);
-
-  const userLocation = document.createElement('p');
-  userLocation.classList.add('card-p');
-  userLocation.textContent = `Location: ${user.location}`; 
-  infoCont.appendChild(userLocation);
-
-  const userUrl = document.createElement('p');
-  userUrl.classList.add('card-p');
-  const userUrls = user.url;
-  userUrl.innerHTML = userUrls.link(user.url);
-  infoCont.appendChild(userUrl);
-
-  const userFollowers = document.createElement('p');
-  userFollowers.classList.add('card-p');
-  userFollowers.textContent = `Followers: ${user.followers}`; 
-  infoCont.appendChild(userFollowers);
-
-  const userFollowing = document.createElement('p');
-  userFollowing.classList.add('card-p');
-  userFollowing.textContent = `Following: ${user.following}`; 
-  infoCont.appendChild(userFollowing);
-
-  const userBio = document.createElement('p');
-  userBio.classList.add('card-p');
-  userBio.textContent = `Bio: ${user.bio}`; 
-  infoCont.appendChild(userBio);
-
-
-
-  //let card = cardCreator(user);
-  let cards = document.querySelector(".cards");
-  cards.appendChild(div);
-  
-  return cards;
-
-}
-
-
-axios.get('https://api.github.com/users/Suffyan-T')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
-
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -102,7 +25,7 @@ axios.get('https://api.github.com/users/Suffyan-T')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -124,56 +47,89 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
-axios.get('https://api.github.com/users/tetondan')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
+//cardCreator Function
+function cardCreator(user) {
+  // div
+  const div = document.createElement('div');
+  div.classList.add('card');
 
-axios.get('https://api.github.com/users/dustinmyers')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
+  // Image
+  const image = document.createElement('img');
+  image.src = user.avatar_url; 
+  div.appendChild(image);
+  image.classList.add('card-img');
+  
+  // Info Card
+  const infoCont = document.createElement('div');
+  infoCont.classList.add('card-info');
+  div.appendChild(infoCont);
 
-axios.get('https://api.github.com/users/navalta3030')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
+  // User name
+  const cardName = document.createElement('h3');
+  cardName.classList.add('card-name');
+  cardName.textContent = user.name;
+  infoCont.appendChild(cardName);
 
-axios.get('https://api.github.com/users/luishrd')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
+  // User Log In
+  const userName = document.createElement('p');
+  userName.classList.add('card-username');
+  userName.textContent = user.login; 
+  infoCont.appendChild(userName);
 
-axios.get('https://api.github.com/users/bigknell')
-.then((axiosData) => {
-  console.log('axiosData.data: ', axiosData.data);
-  cardCreator(axiosData.data)
-})
-.catch((err) => {
-  console.log('error: ', err);
-})
+  // Location
+  const userLocation = document.createElement('p');
+  userLocation.classList.add('card-p');
+  userLocation.textContent = `Location: ${user.location}`; 
+  infoCont.appendChild(userLocation);
+
+  // Url
+  const userUrl = document.createElement('p');
+  userUrl.classList.add('card-p');
+  const userUrls = user.url;
+  userUrl.innerHTML = userUrls.link(user.url);
+  infoCont.appendChild(userUrl);
+
+  // Followers
+  const userFollowers = document.createElement('p');
+  userFollowers.classList.add('card-p');
+  userFollowers.textContent = `Followers: ${user.followers}`; 
+  infoCont.appendChild(userFollowers);
+
+  // Following
+  const userFollowing = document.createElement('p');
+  userFollowing.classList.add('card-p');
+  userFollowing.textContent = `Following: ${user.following}`; 
+  infoCont.appendChild(userFollowing);
+
+  // Bio
+  const userBio = document.createElement('p');
+  userBio.classList.add('card-p');
+  userBio.textContent = `Bio: ${user.bio}`; 
+  infoCont.appendChild(userBio);
+  
+  // Return Div
+  return div;
+}
 
 
+ //Acesses cards class
+ let cards = document.querySelector(".cards");
+ 
+
+ // Axios => execute simultaneous requests 
+axios.all([
+  axios.get('https://api.github.com/users/Suffyan-T'),
+  axios.get('https://api.github.com/users/tetondan'),
+  axios.get('https://api.github.com/users/dustinmyers'),
+  axios.get('https://api.github.com/users/navalta3030'),
+  axios.get('https://api.github.com/users/luishrd'),
+  axios.get('https://api.github.com/users/bigknell')
+])
+.then(res => {
+  //this will be executed only when all requests are complete
+  res.forEach(e => {
+    let newDiv = cardCreator(e.data);
+    cards.appendChild(newDiv);
+    console.log(e.data);
+  });
+});
